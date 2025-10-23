c64140146775a0e2916b2d8eba930a2b58cb09ed
Nahuel Nuñez <nunezn53@gmail.com>
2025-10-22 21:31:24 -0300

    agrego el navbar y el footer de shared pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-
+import { BrowserRouter, Routes, Route} from "react-router"
 import './App.css'
+import Menu from "./components/shared/Menu"
+import Footer from "./components/shared/Footer"
+import Home from "./components/pages/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <BrowserRouter>
+    <Menu></Menu>
+    <main>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </main>
+    <Footer></Footer>
+    </BrowserRouter>
     
+   
     </>
   )
 }
--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,7 +1,23 @@
+import { Navbar, Container, Nav} from 'react-bootstrap';
+import { Link, NavLink } from "react-router";
+
 const Menu = () => {
   return (
     <div>
-      
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to={"/"} className='fs-2' href="#home">Crud Food con React</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+
+            <NavLink to={"/"} className={"nav-link"} >Home</NavLink>
+            <NavLink href="#link">Link</NavLink>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
   )
 }
